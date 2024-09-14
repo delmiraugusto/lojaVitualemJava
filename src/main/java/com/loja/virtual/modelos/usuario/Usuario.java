2854a6a5f757ba77aaaf15116abe0193f8dcec26
AnaCarolinaPGDO <anagross60@gmail.com>
2024-09-14 19:41:42 -0300

    modificacao usuario e cliente

--- src/main/java/com/loja/virtual/modelos/usuario/Usuario.java
+++ src/main/java/com/loja/virtual/modelos/usuario/Usuario.java
@@ -1,4 +1,33 @@
 package com.loja.virtual.modelos.usuario;
 
 public class Usuario {
+    private String nome;
+    private String email;
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void SetNome(String nome) {
+        this.nome = nome;
+    }
+    
+        
+    public String getEmail() {
+        return email;
+    }
+
+    public void SetEmail(String email) {
+        this.email = email;
+    }
+
+public String toString() {
+    return "Nome: " + nome + ", Email: " + email;
 }
+    }
