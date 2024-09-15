b53ca888dce5227e03d3f8c4acd99b73c7a00036
AnaCarolinaPGDO <anagross60@gmail.com>
2024-09-15 15:04:06 -0300

    Modi2

--- src/main/java/com/loja/virtual/modelos/usuario/Usuario.java
+++ src/main/java/com/loja/virtual/modelos/usuario/Usuario.java
@@ -1,23 +1,22 @@
 package com.loja.virtual.modelos.usuario;
 
-import java.util.Date;
-
 
 public class Usuario {
-    private static String nome;
+    private String nome;
     private String email;
+    private String senha;
 
-    public Usuario(String nome, String email) {
+    public Usuario(String nome, String email, String senha) {
         this.nome = nome;
         this.email = email;
-
+        this.senha = senha; 
     }
 
-    public static String getNome() {
+    public String getNome() {
         return nome;
     }
 
-    public void SetNome(String nome) {
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
@@ -25,11 +24,20 @@ public class Usuario {
         return email;
     }
 
-    public void SetEmail(String email) {
+    public void setEmail(String email) {
         this.email = email;
-    }
-
-public String toString() {
-    return "Nome: " + nome + ", Email: " + email;
 }
+
+    public String getSenha() {
+    return senha;
+}
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+}
+
+@Override
+    public String toString() {
+        return "Nome: " + nome + ", Email: " + email + ", Senha: " + senha;
     }
+}
