35e1696375537bb2dcad51999aef00e7cc2e721a
GabrielaCoutoD <gabrielanadar@gmail.com>
2024-09-16 19:48:40 -0300

    Modificações Atributos

--- src/main/java/com/loja/virtual/modelos/cliente/CadastrarCliente.java
+++ src/main/java/com/loja/virtual/modelos/cliente/CadastrarCliente.java
@@ -1,6 +1,28 @@
 package com.loja.virtual.modelos.cliente;
 
+import java.util.Scanner;
+
+import static com.loja.virtual.modelos.cliente.Cliente.clientes;
+
 public class CadastrarCliente {
     public static void cadastrarCliente() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Digite seu email: ");
+        String email = scanner.nextLine();
+        /*System.out.println("Digite endereço: ");
+        String endereco = scanner.nextLine();*/
+        System.out.println("Crie uma senha: ");
+        String senha = scanner.nextLine();
+
+        Cliente cliente = new Cliente();
+        cliente.setNome(nome);
+        cliente.setLogin(email);
+        cliente.setSenha(senha);
+        clientes.add(cliente);
     }
-}
+
+    }
