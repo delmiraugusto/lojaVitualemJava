38ecadfe755ca5a60094c0f16a5d0609cf3db765
Delmir Augusto <delmir19.da@gmail.com>
2024-09-17 18:44:08 -0300

    Realizando a Verificacao de login e senha e criando o CadastrarProdutos

--- src/main/java/com/loja/virtual/modelos/gestor/CadastrarProdutos.java
+++ src/main/java/com/loja/virtual/modelos/gestor/CadastrarProdutos.java
@@ -1,7 +1,32 @@
 package com.loja.virtual.modelos.gestor;
 
-public class CadastrarProdutos {
-    public static void cadastrarProdutos() {
+import java.util.Scanner;
 
+import com.loja.virtual.modelos.produto.Produto;
+
+public class CadastrarProdutos {
+    
+
+    public static void cadastrarProdutos() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Cadastrar Produto");
+        Produto jogo = new Produto();
+        System.out.print("Insira a Descrição: ");
+        String descricao = sc.nextLine();
+        jogo.setDescricao(descricao);
+        System.out.print("Insira o Nome do Produto: ");
+        String nome = sc.nextLine();
+        jogo.setNomeProduto(nome);
+        System.out.print("Insira a Data de Fabricação: ");
+        String dataFabricao = sc.nextLine();
+        jogo.setDataFabricacao(dataFabricao);
+        System.out.print("Insira a Quantidade de estoque: ");
+        int estoque = sc.nextInt();
+        jogo.setQuantidadeEstoque(estoque);
+        System.out.print("Insira o Preço: ");
+        double preco = sc.nextDouble();
+        jogo.setValorUnitario(preco);
+        Produto.produtos.add(jogo);
     }
 }
